976ab77c91b2846d4f8c97148c2a52b9d710bb32
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-05-11 12:19:57 -0700

    add more edge cases for login/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def create
     user = User.create(params.require(:user).permit(:username, :password))
+    if user.save
     session[:user_id] = user.id
     redirect_to '/'
+    else
+      flash[:danger] = "All fields need to be filled correctly"
+      redirect_to '/signup'
 end
-
+end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 5 }
 end
