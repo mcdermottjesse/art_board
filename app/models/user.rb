3ad7a0db0082735d17eaf5d39dca71d070c733f9
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-05-19 13:28:54 -0700

    add more tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true
+  validates :username, length: {minimum: 5}
   validates :password, presence: true
   validates :password, length: { minimum: 5 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-
-  describe "User Validations" do
+  
+  describe "Create User Validations" do
 
   context "needs username and password" do
     it "should create a user" do
@@ -26,8 +25,57 @@ context "if no password" do
   end
 end
 
+context "if password is less than 5 characters" do
+  it "should not create a user" do
+    @user = User.create(username: "Username", password: "Pass")
+    expect(@user).to_not be_valid
+  end
+end
 
+context "if userneame is less than 5 characters" do
+  it "should not create a user" do
+    @user = User.create(username: "User", password: "Password")
+    expect(@user).to_not be_valid
+  end
+end
+end
 
+describe "Valid Login Credentials" do
+
+  context "needs useranme and password" do
+    it "should login user" do
+      @user = User.first_or_create(username: "Username", password: "Password")
+      expect(@user).to be_valid
+    end
+  end
+
+  context "if username is not present" do
+    it "should not login user" do
+    @user = User.first_or_create(username: nil, password: "Password")
+  expect(@user).to_not be_valid
+  end
+  end
+
+  context "if password is not present" do
+    it "should not login user" do
+      @user = User.first_or_create(username: "Username", password: nil)
+      expect(@user).to_not be_valid
+    end
+  end
+
+  context "if username is less than 5 characters" do
+  it "it should not login user" do
+    @user = User.first_or_create(username: "User", password: "Password")
+    expect(@user).to_not be_valid
+  end
+end
+
+context "if password is less than 5 characters" do
+  it "it should not login user" do
+    @user = User.first_or_create(username: "Username", password: "Pass")
+    expect(@user).to_not be_valid
+  end
+end
 
   end
 end
