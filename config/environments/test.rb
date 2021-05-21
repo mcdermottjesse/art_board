a1b26f807f3b801023a26241882191ca2abdf7e0
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-05-21 14:49:04 -0700

    tidy test code

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -52,6 +52,11 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,26 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "User/Visitor navigates to Art home page", type: :feature, js: true do
 
-  before :each do
-    @user = User.create! username:"Test User", password: "Password"
-
-    
+before :each do
+  @user = User.create! username:"Test User", password: "Password"
+ 
   @user.arts.create!(
-    name: "Man & Gun",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1S6zA4AnyLzfpmGJdAio-mOSkDDDCpIxsFw&usqp=CAU",
-    description: "Van Gogh"
+    name: "In Thought",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxAKQ_Wafp303ZqqiDhz7bd9sfyWvCHraaRw&usqp=CAU",
+    description: "Picasso Painting"
   )
-  
 end
+
 scenario "They can see art that has been uploaded by users on the home page" do
  
   visit "/"
 
-  save_screenshot
-
-  expect(page).to have_content('Man & Gun')
-  expect(page).to have_content('Van Gogh')
+  expect(page).to have_content('In Thought')
+  expect(page).to have_content('Picasso Painting')
   expect(page).to have_css 'div.img-index'
 
-end
+  puts page.html
+  save_screenshot
+  end
 end
